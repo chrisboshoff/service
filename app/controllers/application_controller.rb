ec2917bd2eba41b0be99693e2beaa3c805c44fa9
Chris Boshoff <chris@mymobility.co.za>
2014-07-31 09:24:55 +0200

    asset precompile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception  
+  helper_method :current_user, :require_login
   
-  helper_method :current_user
-
+ 
   private
+ 
+  def require_login
+    unless current_user
+      flash[:warning] = "You are not currently logged in"
+      redirect_to log_in_path
+    end
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
