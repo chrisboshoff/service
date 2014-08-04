1d5a7e05419f834c467ee2e032366a5e3d843908
Chris Boshoff <chris@mymobility.co.za>
2014-08-04 08:19:07 +0200

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
-  validates_presence_of :email
+  validates_presence_of :email, :name
   validates_uniqueness_of :email, :case_sensitive => false
   
   def self.authenticate(email, password)
